00e1ed2a8c8fbb640462b2cc57f74616a55bdb66
milk-maid <roqbell@gmail.com>
2023-04-17 16:22:18 +0100

    moving up

--- an_intro/bmi.py
+++ an_intro/bmi.py
@@ -1,5 +1,29 @@
 weight = input("Enter your weight in kg\n")
 height = input("Enter your height in m\n")
 h2 = float(height) * float(height)
-bmi = int(weight) // h2  # to print it without the fractional part
-print("If your weight is " + weight + " and your height is " + height + " your BMI is therefore " + str(bmi) + "kg/m2")
+bmi = int(weight) / h2  # use "//" to print it without the fractional part
+# print("If your weight is " + weight + " and your height is " + height + " your BMI is therefore "
+# + str(bmi) + "kg/m2")
+# aa = str(bmi)
+# print(type(aa))
+rounded = round(bmi, 2)
+# print(type(rounded))
+print(f"If your weight is {weight} and your height is {height} your BMI is therefore = {rounded}kg/m2")
+if bmi < 16:
+    print("You are Severely Underweight")
+elif bmi < 17:
+    print("You are Moderately Underweight")
+elif bmi < 18.5:
+    print("You are mildly Underweight")
+elif bmi < 25:
+    print("You appear NORMAL")
+elif bmi < 30:
+    print("You are OVERWEIGHT(pre-obese)")
+elif bmi < 35:
+    print("You are OBESE(class I)")
+elif bmi < 40:
+    print("You are OBESE(class II)")
+else:
+    print("This is too much OBESITY (class III)")
+
+print("Thanks For Coming Around!!!")
---
+++ an_intro/elif.py
@@ -0,0 +1,15 @@
+num = int(input("Enter any digit below 6: \n"))
+
+if num == 1:
+    print("You entered ONE")
+elif num == 2:
+    print("You entered TWO")
+elif num == 3:
+    print("You entered THREE")
+elif num == 4:
+    print("You entered FOUR")
+elif num == 5:
+    print("You entered FIVE")
+else:
+    print("Always Follow Instructions#")
+print("Thanks For Coming Around!!!")
