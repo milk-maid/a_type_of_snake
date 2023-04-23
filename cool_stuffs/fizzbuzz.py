2727df2f7079ed160b4bcc2df768c87ece45d6e3
milk-maid <roqbell@gmail.com>
2023-04-23 08:53:26 +0100

    fizzbuzz update

--- cool_stuffs/fizzbuzz.py
+++ cool_stuffs/fizzbuzz.py
@@ -4,10 +4,10 @@ a = range(1, 101)
 for i in a:
     if i % 15 == 0:
         print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
     else:
         print(i)
 print("Welcome!")
